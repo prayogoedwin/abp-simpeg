6116c417c34026a655543edec64d04e198ef5c88
Edwin <prayogo.edwin@gmail.com>
2026-01-06 13:34:35 +0700

    update reset device

--- app/Filament/Resources/MemberResource/Pages/EditMember.php
+++ app/Filament/Resources/MemberResource/Pages/EditMember.php
@@ -15,6 +15,31 @@ class EditMember extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('resetDevice')
+                ->label('Reset Device')
+                ->icon('heroicon-o-device-phone-mobile')
+                ->color('warning')
+                ->requiresConfirmation()
+                ->modalHeading('Reset Device')
+                ->modalDescription(fn () => $this->record->identity_name 
+                    ? "Yakin ingin reset device \"{$this->record->identity_name}\" dari akun {$this->record->name}?" 
+                    : "Akun ini belum terdaftar di device manapun."
+                )
+                ->modalSubmitActionLabel('Ya, Reset')
+                ->visible(fn () => $this->record->identity !== null)
+                ->action(function () {
+                    $this->record->update([
+                        'identity' => null,
+                        'identity_name' => null,
+                        'device_name' => null,
+                    ]);
+
+                    Notification::make()
+                        ->title('Device Berhasil Direset')
+                        ->body("Device untuk {$this->record->name} telah direset. Pegawai dapat login dari perangkat baru.")
+                        ->success()
+                        ->send();
+                }),
             ViewAction::make(),
             DeleteAction::make(),
         ];
